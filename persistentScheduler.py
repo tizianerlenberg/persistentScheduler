ccd41544ea11aa1cd2e3e78bb3cc515302c0eef7
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-09 17:18:14 +0200

    changed json file content management

--- persistentScheduler.py
+++ persistentScheduler.py
@@ -50,16 +50,33 @@ class Scheduler:
                 myFile.write(json.dumps(self.fileDict))
 
         with open(self.file, "r") as myFile:
-            self.fileDict = json.loads(myFile.read())
+            content= myFile.read()
+            if content != "":
+                self.fileDict = json.loads(content)
+            else:
+                self.fileDict = {}
+            for key in self.fileDict.keys():
+                self.fileDict[key]["last"]= strToTime(self.fileDict[key]["last"])
+
     def updateFile(self):
         if self.file != Path():
             with open(self.file, "w") as myFile:
-                self.fileDict= self.dict
-                myFile.write(json.dumps(self.fileDict))
+                self.fileDict= self.dict.copy()
+                for key in self.fileDict.keys():
+                    self.fileDict[key]= self.fileDict[key].copy()
+                    self.fileDict[key].pop('interval')
+                    self.fileDict[key].pop('function')
+                    self.fileDict[key]["last"]= self.fileDict[key]["last"]
+                tmpDict= self.fileDict.copy()
+                for key in tmpDict.keys():
+                    tmpDict[key]= tmpDict[key].copy()
+                    tmpDict[key]["last"]= timeToStr(tmpDict[key]["last"])
+                myFile.write(json.dumps(tmpDict))
         else:
             raise Exception("File not specified in Constructor")
-    def addTask(self, name, interval, function, group="", last=timeToStr(getTime())):
-        self.dict[name]= {"interval": deltaToStr(interval), "last": last, "function": function.__name__}
+
+    def addTask(self, name, interval, function, group="", last=getTime()):
+        self.dict[name]= {"interval": interval, "last": last, "function": function.__name__}
         self.functions[function.__name__]= function
     def addTaskIfNotExists(self, name, interval, function, group=""):
         if not (name in self.fileDict.keys()):
@@ -70,10 +87,10 @@ class Scheduler:
         self.dict.pop(name)
     def runPending(self):
         for d in self.dict:
-            if (getTime() - strToTime(self.dict[d]["last"])) > strToDelta(self.dict[d]["interval"]):
+            if (getTime() - self.dict[d]["last"]) > (self.dict[d]["interval"]):
                 print(f"doing {d}")
                 self.functions[self.dict[d]["function"]]()
-                self.dict[d]["last"]= timeToStr(datetime.datetime.now())
+                self.dict[d]["last"]= getTime()
     def runPendingAndUpdateFile(self):
         for value in range(self.fileUpdateIntervalCursor, self.fileUpdateInterval):
             self.fileUpdateIntervalCursor += 1
@@ -88,7 +105,7 @@ def hello():
 
 def main():
     test= Scheduler(file="test.json")
-    test.addTaskIfNotExists("task1", datetime.timedelta(seconds=20), hello)
+    test.addTaskIfNotExists("task1", datetime.timedelta(seconds=5), hello)
     test.updateFile()
     while True:
         test.runPendingAndUpdateFile()
