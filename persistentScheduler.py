8d2842fb11224c22a4bfefd21fb12007beb1b181
Tizian Erlenberg <accounts@erlenberg.net>
2022-05-09 18:35:10 +0200

    added threading

--- persistentScheduler.py
+++ persistentScheduler.py
@@ -14,9 +14,6 @@ import queue
 from pathlib import Path, PurePosixPath
 from random import randint
 
-DISPATCHER = {}
-COMPLETED = {}
-
 def getTime():
     return datetime.datetime.now()
 
@@ -34,12 +31,21 @@ def strToDelta(input):
     zerodate= datetime.datetime(1, 1, 1)
     return strToTime(input) - zerodate
 
+def runFuntion(taskQueue, resultQueue):
+    task= taskQueue.get()
+    task[1]()
+    time.sleep(5)
+    resultQueue.put(task[0])
+
 class Scheduler:
     def __init__(self, file=Path(), fileUpdateInterval=1):
         self.dict= {}
         self.fileDict= {}
         self.fileUpdateInterval= fileUpdateInterval
         self.fileUpdateIntervalCursor= 0
+        self.threadingDispatcher = {}
+        self.threadingCompleted = {}
+        self.runningThreads = {}
 
         if isinstance(file, str):
             self.file= Path(file)
@@ -71,20 +77,28 @@ class Scheduler:
             raise Exception("File not specified in Constructor")
 
     def addTask(self, name, interval, function, group="", last=getTime()):
-        self.dict[name]= {"interval": interval, "last": last, "function": function}
+        self.dict[name]= {"interval": interval, "last": last, "function": function, "group": group}
+        self.threadingDispatcher[group]= queue.Queue()
+        self.threadingCompleted[group]= queue.Queue()
+        self.threadingCompleted[group].put(name)
     def addTaskIfNotExists(self, name, interval, function, group=""):
         if not (name in self.fileDict.keys()):
-            self.addTask(name, interval, function)
+            self.addTask(name, interval, function, group=group)
         else:
-            self.addTask(name, interval, function, last=self.fileDict[name]['last'])
+            self.addTask(name, interval, function, last=self.fileDict[name]['last'], group=group)
     def removeTask(self, name):
         self.dict.pop(name)
     def runPending(self):
         for key in self.dict.keys():
             if (getTime() - self.dict[key]["last"]) > (self.dict[key]["interval"]):
-                print(f"doing {key}")
-                self.dict[key]["function"]()
-                self.dict[key]["last"]= getTime()
+                currentDispQueue= self.threadingDispatcher[self.dict[key]['group']]
+                currentCompQueue= self.threadingCompleted[self.dict[key]['group']]
+                if currentCompQueue.qsize() > 0:
+                    completedTask = currentCompQueue.get()
+                    self.dict[completedTask]["last"]= getTime()
+                    currentDispQueue.put([key, self.dict[key]['function']])
+                    self.runningThreads[self.dict[key]['group']] = threading.Thread(target=runFuntion, args=(currentDispQueue, currentCompQueue,))
+                    self.runningThreads[self.dict[key]['group']].start()
     def runPendingAndUpdateFile(self):
         for value in range(self.fileUpdateIntervalCursor, self.fileUpdateInterval):
             self.fileUpdateIntervalCursor += 1
@@ -96,13 +110,17 @@ class Scheduler:
 
 def hello():
     print("hello")
+    pass
 
 def main():
     test= Scheduler(file="test.json")
-    test.addTaskIfNotExists("task1", datetime.timedelta(seconds=5), hello)
+    test.addTaskIfNotExists("task1", datetime.timedelta(seconds=1), hello, group="task1")
+    test.addTaskIfNotExists("task2", datetime.timedelta(seconds=4), hello, group="task2")
     test.updateFile()
     while True:
         test.runPendingAndUpdateFile()
+        #print(test.threadingDispatcher)
+        print(threading.active_count())
         time.sleep(1)
 
 if __name__ == '__main__':
